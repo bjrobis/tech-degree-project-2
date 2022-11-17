a13d7981163805707633127a17070e3d3215760e
Brandon_Robison <bjrobis@gmail.com>
2022-11-17 10:51:07 -0700

    added the event listener

--- data-pagination-and-filtering-v1/js/script.js
+++ data-pagination-and-filtering-v1/js/script.js
@@ -11,12 +11,7 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
+// This function will create and append the elements needed to display a page of nine students
 function showPage(list, page) {
    let startIndex = (page * 9) - 9;
    let endIndex = page * 9;
@@ -41,14 +36,12 @@ function showPage(list, page) {
       }
    }  
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+//This function will create and insert/append the elements needed for the pagination buttons
+
+let linkList = document.querySelector('.link-list');
 function addPagination(list) {
    let numOfPages = Math.ceil(list.length / 9);
-   let linkList = document.querySelector('.link-list');
-   linkList = '';
+   linkList.innerHTML = '';
    for (i = 1; i <= numOfPages; i++) {
       let button = `
          <li>
@@ -56,19 +49,24 @@ function addPagination(list) {
          </li>
       `
       linkList.insertAdjacentHTML('beforeend', button);
-      button.className = 'active';
-   
-       }
+      }
+   let firstButton = linkList.querySelector('button');
+   firstButton.className = 'active';
    }
- 
 
-  // create an event listener on the `link-list` element
-    // if the click target is a button:
-      // remove the "active" class from the previous button
-      // add the active class to the clicked button
-      // call the showPage function passing the `list` parameter and page to display as arguments
+//This is an event listener for the link buttons created by the addPagination function.
+//When a link button is clicked it loads the appropriate page and changes the class of the most recently clicked button to active
+linkList.addEventListener('click', (e) => {
+   let selectedButton = e.target;
+   if (selectedButton.tagName === 'BUTTON') {
+      let activeButton = linkList.querySelector('.active');
+      activeButton.className = '';
+      selectedButton.className = 'active';
+      showPage(data, selectedButton.textContent);
+   }
 
+})
 
-
-
-// Call functions
+//Calling the functions created above
+showPage(data, 1);
+addPagination(data);
